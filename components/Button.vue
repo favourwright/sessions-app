6ce917324483358fae0d1c728c7bbcef000d2624
favourwright <gismozfexie@yahoo.com>
2022-03-21 22:56:25 +0100

    progress

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="py-[10] lg:py-[10px] px-[10px] lg:px-[20px]
+  <button class="py-[10px] lg:py-[10px] px-[14px] lg:px-[20px]
     flex justify-center relative
     rounded-full lg:min-w-[164px] whitespace-nowrap">
     <slot></slot>
