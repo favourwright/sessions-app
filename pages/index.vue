806e1e1c5a43356f92c3004db4dcfc21e1fd0278
favourwright <gismozfexie@yahoo.com>
2022-03-23 20:33:09 +0100

    progress...

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,9 @@
   <div class="flex relative h-full overflow-hidden">
     <div
       class="absolute flex items-center h-full
-      transition duration-1000 ease-in-out"
-      :style="{ left: `${container_left}px` }">
+      transition duration-1000 ease-in-out left-0"
+      :class="{slide:slide}"
+      style="--left: -500px;">
       <div class="px-[5%] lg:px-[192px]">
         <h1 class="text-[42px] lg:text-[64px] mb-[33px] font-black text-light-blue">Sessions</h1>
         <Search class="w-full lg:w-[640px] mb-[33px]" name="Search for event..." />
@@ -16,6 +17,15 @@
   </div>
 </template>
 
+<style scoped>
+.slide{
+  animation: mymove 5s 1;
+}
+@keyframes mymove {
+  50% {left: var(--left);}
+}
+</style>
+
 <script>
 import Layout from "@/layouts/default"
 export default {
@@ -24,29 +34,11 @@ export default {
   },
   name: "IndexPage",
   data: () => ({
-    container_left: 1,
+    slide: false,
   }),
   methods: {
-    async Handle() {
-      // let interval = 0
-      // let px = 500
-      // let vm = this
-      // await (async ()=>{
-      //   let test = setInterval(() => {
-      //     interval++
-      //     vm.container_left = -(interval * (1 + interval / px))
-      //     interval == 450 ? clearInterval(test) : null
-      //   }, 0)
-      //   return true
-      // })()
-      // await (async ()=>{
-      //   let test = setInterval(() => {
-      //     interval++
-      //     vm.container_left = -(interval * (1 + interval / px))
-      //     interval == 500 ? clearInterval(test) : null
-      //   }, 8)
-      //   return true
-      // })()
+    Handle() {
+      this.slide=true
     },
   },
 }
