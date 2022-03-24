e783db5db5cd7b3249c1c34a3c085836e38ddb6a
favourwright <gismozfexie@yahoo.com>
2022-03-24 17:42:16 +0100

    progress... continue building-out page

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,45 @@
 <template>
   <div class="flex relative h-full overflow-hidden">
     <div
-      class="absolute h-full flex items-center
-      transition duration-1000 ease-in-out left-0"
-      :class="{slide:slide}"
+      class="landing absolute h-full flex items-center ease-in-out left-0"
+      :class="{slide}"
       :style="`--left: ${slide_amount}px;`">
-      <div ref="landing_1"
-        class="px-[5%] lg:px-[192px] h-full flex flex-col justify-center
-        float-left">
-        <h1 class="text-[42px] lg:text-[64px] mb-[33px] font-black text-light-blue">Sessions</h1>
-        <Search class="w-full lg:w-[640px] mb-[33px]" name="Search for event..." />
-        <h2 class="text-[26px] lg:text-[40px] text-gray-70 leading-[140%] font-semibold">
-          Here are some tailored events we made, <span class="text-white">just for you.</span>
-        </h2>
-      </div>
+      <vue-scroll :ops="{ scrollPanel: { scrollingY: true } }">
+        <div ref="landing_1"
+          class="px-[5%] lg:px-[192px] pt-[50px] lg:pt-[77px] h-full
+          flex flex-col justify-center float-left">
+          <h1 class="text-[42px] lg:text-[64px] mb-[33px] font-black text-light-blue">Sessions</h1>
+          <Search class="w-full lg:w-[640px] mb-[33px]" name="Search for event..." />
+          <h2 class="text-[26px] lg:text-[40px] text-gray-70 leading-[140%] font-semibold">
+            Here are some tailored events we made, <span class="text-white">just for you.</span>
+          </h2>
+        </div>
+      </vue-scroll>
       <div
         ref="landing_2"
-        class="min-w-[500px] h-full bg-white/10 float-left"
-        @click="slide=!slide"></div>
+        class="min-w-[500px] h-full float-left flex">
+          <div class="flex-grow-0 flex items-center text-white">
+            <div
+              @click="slide=!slide"
+              class="transform transition duration-300 ease-in-out"
+              :class="{rotate:landing_2_is_visible}">
+              <iconly class="ico" name="arrow-left2" size="48" />
+            </div>
+          </div>
+          <div class="flex-grow bg-white/10">
+            <h1>Hi from ere</h1>
+          </div>
+        </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .slide{
-  animation: mymove 3s 1 forwards;
+  animation: mymove 2s 1 forwards;
+}
+.rotate{
+  @apply -rotate-180;
 }
 @keyframes mymove {
   100% {transform: translateX(var(--left));}
@@ -38,10 +53,13 @@ export default {
     Layout,
   },
   name: "IndexPage",
-  data: () => ({
-    slide: false,
-    slide_amount:0,
-  }),
+  data(){
+    return {
+      slide: false,
+      slide_amount:0,
+      landing_2_is_visible:false,
+    }
+  },
   methods:{
     getSize(){
       return this.$refs.landing_1.clientWidth
@@ -58,20 +76,18 @@ export default {
           if (entry.isIntersecting) {
             let elem = entry.target;
             console.log(elem);
+            this.landing_2_is_visible=true
           }
         });
       }, options);
 
       let target = this.$refs.landing_2;
       observer.observe(target);
-    }
+    },
   },
   mounted(){
     this.slide_amount = -this.getSize()
-
-    setTimeout(() => {
-      this.iO()
-    }, 1000);
+    this.iO()
   }
 }
 </script>
