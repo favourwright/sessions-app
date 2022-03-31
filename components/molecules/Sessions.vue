e2c845954e775b0519fa47dc0c2d110633211c08
favourwright <gismozfexie@yahoo.com>
2022-03-31 18:15:59 +0100

    progress

--- components/molecules/Sessions.vue
+++ components/molecules/Sessions.vue
@@ -2,15 +2,15 @@
 <div class="grid grid-cols-3 gap-4 rounded-2xl lg:rounded-3xl bg-white/10 p-6 lg:p-8">
   <div class="bg-white/10 rounded-2xl p-4">
     <h4>Days</h4>
-    <h2 class="text-5xl text-white" v-text="days"></h2>
+    <h2 class="text-3xl lg:text-5xl text-white" v-text="days"></h2>
   </div>
   <div class="bg-white/10 rounded-2xl p-4">
     <h4>Hours</h4>
-    <h2 class="text-5xl text-white" v-text="hours"></h2>
+    <h2 class="text-3xl lg:text-5xl text-white" v-text="hours"></h2>
   </div>
   <div class="bg-white/10 rounded-2xl p-4">
     <h4>Minutes</h4>
-    <h2 class="text-5xl text-white" v-text="minutes"></h2>
+    <h2 class="text-3xl lg:text-5xl text-white" v-text="minutes"></h2>
   </div>
 </div>
 </template>
